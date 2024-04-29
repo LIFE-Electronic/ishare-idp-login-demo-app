65b198b99e2503563eb27983ca1d476110342e81
MP <markus@life-electronic.nl>
2024-04-29 12:10:40 +0200

    copy

--- src/component/PolicyStore.js
+++ src/component/PolicyStore.js
@@ -8,12 +8,19 @@ export function PolicyStore() {
   const [backendUrl, setBackendUrl] = useState("http://localhost:3000");
   const redirectUrl = encodeURIComponent(`${getBaseUrl()}/policy-store`);
 
+  console.log(redirectUrl)
+  console.log(token)
+
+  const copyToClipboard = (text) => async () => {
+    await navigator.clipboard.writeText(text);
+}
+
   return (
     <section>
       <h1>Policy Store</h1>
 
       <form>
-        <label>Backend url</label>
+        <label style={{"paddingRight": "20px"}}>Backend url</label>
         <input value={backendUrl} onChange={e => setBackendUrl(e.target.value)}  />
       </form>
 
@@ -23,7 +30,9 @@ export function PolicyStore() {
         token && (
           <div style={{ maxWidth: 440 }}>
             <h2>Token</h2>
-            <pre style={{ overflowWrap: "break-word" }}>{token}</pre>
+            <textarea disabled cols="50" value={token}></textarea>
+            <br></br>
+            <button onClick={copyToClipboard(token)}>Copy</button>
           </div>
         )
       }
